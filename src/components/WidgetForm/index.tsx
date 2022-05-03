ab1f0c9a73944dc869475692166e20b52a6c293b
Williams25 <william.souza.timao@hotmail.com>
2022-05-03 18:17:34 -0300

    iniciando form

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,7 @@
+export const WidgetForm = () => {
+  return (
+    <div className="bg-zinc-800 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      helo
+    </div>
+  );
+};
