1efe0ec5b2d92b3100775c7f3b69682c7e616367
Williams25 <william.souza.timao@hotmail.com>
2022-05-03 20:52:59 -0300

    criando regra de como funciona o popover de feedback

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,7 +1,68 @@
+import { useState } from "react";
+import { Footer } from "../Footer";
+
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: "/icons/Bug.svg",
+      alt: "imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: "/icons/Emoji.svg",
+      alt: "imagem de uma lampada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: "/icons/Thought.svg",
+      alt: "imagem de um balão de pensamento",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
 export const WidgetForm = () => {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSend, setFeedbackSend] = useState<boolean>(false);
+
+  const handleRestartFeedback = () => {
+    setFeedbackSend(false);
+    setFeedbackType(null);
+  };
+  const handleFeedBackSend = () => setFeedbackSend(true);
+
   return (
     <div className="bg-zinc-800 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      helo
+      {!feedbackSend && (
+        <>
+          {!feedbackType && (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          )}
+
+          {feedbackType && (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedBackRestarted={handleRestartFeedback}
+              onFeedBackSend={handleFeedBackSend}
+            />
+          )}
+        </>
+      )}
+
+      {feedbackSend && (
+        <FeedbackSuccessStep onFeedBackRestarted={handleRestartFeedback} />
+      )}
+      <Footer />
     </div>
   );
 };
