1f0de17c89ff6a5ac2c6fa17089ed2fb61af32b9
Williams25 <william.souza.timao@hotmail.com>
2022-05-03 20:51:13 -0300

    criando footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className="text-xs text-neutral-400">
+      Feito com ♥ pela{" "}
+      <a href="#" className="underline underline-offset-2">
+        Rocketseat
+      </a>
+    </footer>
+  );
+};
